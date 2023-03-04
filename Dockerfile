398034a09d02db554a5be25697774c828016802d
Ilanga <Ilanga@git.com>
2023-03-04 16:30:59 +0600

    Starts at 127.0.0.1:8000

--- Dockerfile
+++ Dockerfile
@@ -8,22 +8,22 @@ ENV PATH "$PATH:/mnt/d/PyCharm/ytd/YTD_deploy_1509"
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-# install psycopg2 dependencies
-RUN apk update \
-    && apk add postgresql-dev gcc python3-dev musl-dev
+## install psycopg2 dependencies
+#RUN apk update \
+#    && apk add postgresql-dev gcc python3-dev musl-dev
 
 # install dependencies
 RUN pip install --upgrade pip
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-# copy entrypoint
-COPY entrypoint .
-RUN sed -i 's/\r$//g' entrypoint
-RUN chmod +x entrypoint
+## copy entrypoint
+#COPY entrypoint .
+#RUN sed -i 's/\r$//g' entrypoint
+#RUN chmod +x entrypoint
 
 # copy projec
 COPY . .
 
-# run entrypoint
-ENTRYPOINT ["sh", "entrypoint"]
+## run entrypoint
+#ENTRYPOINT ["sh", "entrypoint"]
--- YouTubeAudio/settings.py
+++ YouTubeAudio/settings.py
@@ -83,27 +83,27 @@ WSGI_APPLICATION = 'YouTubeAudio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ytd_psql',
-        'USER': 'postgres_ytd',
-        'PASSWORD': 'PuKAxU9JdZiwQrS',
-        'HOST': 'postgres',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-#         "USER": os.environ.get("SQL_USER", "user"),
-#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#         "HOST": os.environ.get("SQL_HOST", "localhost"),
-#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ytd_psql',
+#         'USER': 'postgres_ytd',
+#         'PASSWORD': 'PuKAxU9JdZiwQrS',
+#         'HOST': 'db',
+#         'PORT': 5432,
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
